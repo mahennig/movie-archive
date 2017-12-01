4c451a12a0635c74a4dd04381747a65aedcc0c83
Markus Hennig <hennig_markus@gmx.de>
2017-12-01 16:31:38 +0100

    autowired databank

--- moviearchive/src/main/java/de/hennig/moviearchive/userinterface/components/MovieTable.java
+++ moviearchive/src/main/java/de/hennig/moviearchive/userinterface/components/MovieTable.java
@@ -1,29 +1,47 @@
 package de.hennig.moviearchive.userinterface.components;
 
 import com.google.common.collect.Lists;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Grid;
 import de.hennig.moviearchive.domain.Movie;
 import de.hennig.moviearchive.repositories.MovieRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringComponent
 @UIScope
+@SuppressWarnings("serial")
 public class MovieTable extends Grid<Movie> {
 
-    @Autowired
-    MovieRepository movieRepository;
+	@Autowired
+	MovieRepository movieRepository;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public MovieTable() {
-        this.addColumn(Movie::getTitle).setCaption("Titel");
-        this.addColumn(Movie::getYear).setCaption("Jahr");
-        this.addColumn(Movie::getRunningTime).setCaption("Laufzeit");
+	public MovieTable() {
+		loadApplicationContext();
+		this.addColumn(Movie::getTitle).setCaption("Titel");
+		this.addColumn(Movie::getYear).setCaption("Jahr");
+		this.addColumn(Movie::getRunningTime).setCaption("Laufzeit");
+		this.addColumn(Movie::getDescription).setCaption("Handlung");
 
-        List<Movie> movies = new ArrayList();
-        movies = Lists.newArrayList(movieRepository.findAll());
-    }
+		List<Movie> movies = new ArrayList();
+		movies = Lists.newArrayList(movieRepository.findAll());
+		movies.forEach(e -> logger.info(e.getTitle()));
+		this.setItems(movies);
+	}
+
+	private void loadApplicationContext() {
+		this.movieRepository = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(VaadinServlet.getCurrent().getServletContext())
+				.getBean(MovieRepository.class);
+	}
 }
--- moviearchive/src/main/java/de/hennig/moviearchive/userinterface/layouts/MovieEditor.java
+++ moviearchive/src/main/java/de/hennig/moviearchive/userinterface/layouts/MovieEditor.java
@@ -2,6 +2,7 @@ package de.hennig.moviearchive.userinterface.layouts;
 
 import com.vaadin.data.Binder;
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
@@ -9,6 +10,7 @@ import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -18,83 +20,110 @@ import de.hennig.moviearchive.repositories.MovieRepository;
 import de.hennig.moviearchive.userinterface.components.CountryComboBox;
 import de.hennig.moviearchive.userinterface.components.GenreSelect;
 import de.hennig.moviearchive.userinterface.components.YearComboBox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @SpringComponent
 @UIScope
 public class MovieEditor extends VerticalLayout {
 
-    private TextField titleField = new TextField("Titel");
-    ComboBox<Director> directorBox = new ComboBox<>("Regisseur");
-    private HorizontalLayout upperItemContainer = new HorizontalLayout();
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private TextArea descriptionArea = new TextArea("Handlung");
+	private TextField titleField = new TextField("Titel");
+	ComboBox<Director> directorBox = new ComboBox<>("Regisseur");
+	private HorizontalLayout upperItemContainer = new HorizontalLayout();
 
-    private GenreSelect genreSelector = new GenreSelect("Genre");
+	private TextArea descriptionArea = new TextArea("Handlung");
 
-    private TextField runningTime = new TextField("Laufzeit");
-    private HorizontalLayout middleItemContainer = new HorizontalLayout();
+	private GenreSelect genreSelector = new GenreSelect("Genre");
 
-    ComboBox countryComboBox = new CountryComboBox("Land");
-    YearComboBox yearComboBox = new YearComboBox("Erscheinungsjahr");
-    private HorizontalLayout lowerItemContainer = new HorizontalLayout();
+	private TextField runningTime = new TextField("Laufzeit");
+	private HorizontalLayout middleItemContainer = new HorizontalLayout();
 
+	CountryComboBox countryComboBox = new CountryComboBox("Land");
+	YearComboBox yearComboBox = new YearComboBox("Erscheinungsjahr");
+	private HorizontalLayout lowerItemContainer = new HorizontalLayout();
 
-    private Button saveButton = new Button("Speichern", VaadinIcons.SAFE);
-    private Button deleteButton = new Button("Löschen", VaadinIcons.DEL);
-    private HorizontalLayout buttonContainer = new HorizontalLayout();
+	private Button saveButton = new Button("Speichern", VaadinIcons.SAFE);
+	private Button deleteButton = new Button("Löschen", VaadinIcons.CLOSE);
+	private HorizontalLayout buttonContainer = new HorizontalLayout();
 
-    @Autowired
-    MovieRepository movieRepository;
-    private Binder<Movie> movieBinder = new Binder<>(Movie.class);
-    private Movie selectedMovie;
+	@Autowired
+	MovieRepository movieRepository;
 
+	private Binder<Movie> movieBinder = new Binder<>(Movie.class);
+	private Movie selectedMovie = new Movie();
 
-    public MovieEditor() {
-        configComponents();
-        buildLayout();
+	public MovieEditor() {
+		loadApplicationContext();
+		configComponents();
+		buildLayout();
+		initBinder();
 
-        saveButton.addClickListener(e -> movieRepository.save(selectedMovie));
-        deleteButton.addClickListener(e -> movieRepository.delete(selectedMovie));
-    }
+		saveButton.addClickListener(e -> saveMovie(selectedMovie));
+		deleteButton.addClickListener(e -> movieRepository.delete(selectedMovie));
+	}
 
-    private void initBinder(){
+	private void saveMovie(Movie selectedMovie) {
+		if (movieBinder.validate().isOk()) {
+			if (selectedMovie != null) {
+				movieRepository.save(selectedMovie);
+				logger.info("Stored new Movie: " + selectedMovie);
+				logger.info(movieRepository.count() + " movies in database.");
+			} else {
+				new Notification("Bitte füllen Sie zunächst die Felder aus.");
+				logger.info("Movie is null");
+			}
+		}
+	}
 
-    }
+	private void initBinder() {
+		movieBinder.setBean(selectedMovie);
+		movieBinder.forField(titleField).bind(Movie::getTitle, Movie::setTitle);
+	}
 
-    private void configComponents() {
-        titleField.setIcon(VaadinIcons.MOVIE);
-        directorBox.setIcon(VaadinIcons.USER);
+	private void configComponents() {
+		titleField.setIcon(VaadinIcons.MOVIE);
+		directorBox.setIcon(VaadinIcons.USER);
 
-        descriptionArea.setIcon(VaadinIcons.BOOK);
-        descriptionArea.setSizeFull();
-        descriptionArea.setWidth(this.getWidth()*3.8f, Unit.PIXELS);
-        genreSelector.setHeight(100, Unit.PIXELS);
-    }
+		descriptionArea.setIcon(VaadinIcons.BOOK);
+		descriptionArea.setSizeFull();
+		descriptionArea.setWidth(this.getWidth() * 3.8f, Unit.PIXELS);
+		genreSelector.setHeight(100, Unit.PIXELS);
+	}
 
-    private void buildLayout() {
-        upperItemContainer.addComponent(titleField);
-        upperItemContainer.addComponent(directorBox);
-        this.addComponent(upperItemContainer);
+	private void buildLayout() {
+		upperItemContainer.addComponent(titleField);
+		upperItemContainer.addComponent(directorBox);
+		this.addComponent(upperItemContainer);
 
-        this.addComponent(descriptionArea);
-        this.setComponentAlignment(descriptionArea, Alignment.MIDDLE_CENTER);
-        this.addComponent(genreSelector);
+		this.addComponent(descriptionArea);
+		this.setComponentAlignment(descriptionArea, Alignment.MIDDLE_CENTER);
+		this.addComponent(genreSelector);
 
-        middleItemContainer.addComponent(runningTime);
-        this.addComponent(middleItemContainer);
+		middleItemContainer.addComponent(runningTime);
+		this.addComponent(middleItemContainer);
 
-        lowerItemContainer.addComponent(countryComboBox);
-        lowerItemContainer.addComponent(yearComboBox);
-        this.addComponent(lowerItemContainer);
+		lowerItemContainer.addComponent(countryComboBox);
+		lowerItemContainer.addComponent(yearComboBox);
+		this.addComponent(lowerItemContainer);
 
-        buttonContainer.addComponent(saveButton);
-        buttonContainer.addComponent(deleteButton);
-        this.addComponent(buttonContainer);
-        this.setMargin(new MarginInfo(false, false, false, true));
-    }
+		buttonContainer.addComponent(saveButton);
+		buttonContainer.addComponent(deleteButton);
+		this.addComponent(buttonContainer);
+		this.setMargin(new MarginInfo(false, false, false, true));
+	}
 
-    public final void editMovie(Movie movie){
+	public final void editMovie(Movie movie) {
 
-    }
+	}
+
+	private void loadApplicationContext() {
+		this.movieRepository = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(VaadinServlet.getCurrent().getServletContext())
+				.getBean(MovieRepository.class);
+	}
 }
